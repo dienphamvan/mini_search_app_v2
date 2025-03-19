6b5dd6245efc673c4a41e597faa66b1901d169c1
dienpv <chuanthanh133@gmail.com>
2025-03-19 15:10:12 +0700

    feature: add Header component with LionIcon to HomePage

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { LionIcon } from '../common/icons/LionIcon'
+
+export function Header() {
+    return (
+        <div className=' bg-gray-300 py-1'>
+            <div className=' max-w-6xl flex items-center gap-2 mx-auto'>
+                <LionIcon />
+                <span className='text-[10px]'>
+                    <span className='font-extralight'>
+                        An Official Website of the{' '}
+                    </span>
+                    <span>Singapore Government</span>
+                </span>
+            </div>
+        </div>
+    )
+}
--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,8 +1,10 @@
+import { Header } from '~/components/Header'
 import { SearchForm } from '../components/SearchForm'
 
 export function HomePage() {
     return (
         <main className='min-h-screen bg-gray-100'>
+            <Header />
             <SearchForm />
         </main>
     )
